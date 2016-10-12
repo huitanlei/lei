eefec0b374d7035cf962ca4e2f4b66eacc8d942a
Xamarin Class <huitan.lei@gmail.com>
2016-10-12 14:04:20 +0800

    init pro

---
+++ iOS/web/MyWebController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using UIKit;
+
+namespace lei.iOS
+{
+	public partial class MyWebController : UIViewController
+	{
+		public MyWebController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		//moveToMenuViewSegue
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
---
+++ lei/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace lei
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
